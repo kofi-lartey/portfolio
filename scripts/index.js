db11161e26c03bbef0a7fcbe1175546957eb1674
kofi-lartey <kofilartey12@gmail,com>
2025-05-20 02:35:51 -0700

    style

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+// declare variable to store navbar element
+const navElement = document.getElementById('navbar');
+// detect scroll event on document
+document.addEventListener('scroll', function() {
+    // console.log(window.scrollY);
+    if (window.scrollY > 70) {
+        // change navbar bg to blue
+        navElement.style.backgroundColor = '#0979E4';
+    } else {
+        // change navbar bg to transparent
+        navElement.style.backgroundColor = 'transparent';
+    }
+});
